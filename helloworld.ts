7785c25ecd2330e92b186def09d15b4608a53345
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2022-07-20 23:19:31 -0300

    arrays e funcoes

--- helloworld.ts
+++ helloworld.ts
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 var nome:string = "Beto"
 var age:number
 var boolTest:boolean
@@ -16,6 +10,31 @@ console.log(nome);
 console.log(age);
 console.log(boolTest);
 
+/*------------------------------*/
+
+var arrTestes: string[]
+var testes: any //qualquer tipo de variável
+
+testes = 123
+
+arrTestes = ["Guilherme, João"]
+
+console.log(arrTestes[0]);
+console.log(testes);
+
+/*-------------------------------*/
+
+function teste(valor:string){
+    console.log(valor);
+    
+}
+
+function somar(v1:number, v2:number){
+    return v1 + v2
+}
+
+teste(10)
+console.log(somar(20,5));
 
 
 
