b3a43e60df64ae19dee5a5107dc49a09d938e9ae
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2022-07-21 21:46:06 -0300

    organizando arquivos

--- helloworld.js
+++ helloworld.js
@@ -6,19 +6,19 @@ age = 23;
 console.log(nome);
 console.log(age);
 console.log(boolTest);
-/*------------------------------*/
+//////////////////////////////
 var arrTestes;
 var testes; //qualquer tipo de variável
 testes = 123;
 arrTestes = ["Guilherme, João"];
 console.log(arrTestes[0]);
 console.log(testes);
-/*-------------------------------*/
+/////////////////////////////
 function teste(valor) {
     console.log(valor);
 }
 function somar(v1, v2) {
     return v1 + v2;
 }
-teste(10);
+teste(10); // teste para erro no teminal
 console.log(somar(20, 5));
--- helloworld.ts
+++ helloworld.ts
@@ -1,40 +1,46 @@
-// var nome:string = "Beto"
-// var age:number
-// var boolTest:boolean
+var nome:string = "Beto"
+var age:number
+var boolTest:boolean
 
-// boolTest = true
+boolTest = true
 
-// age = 23
+age = 23
 
-// console.log(nome);
-// console.log(age);
-// console.log(boolTest);
+console.log(nome);
+console.log(age);
+console.log(boolTest);
 
-// /*------------------------------*/
 
-// var arrTestes: string[]
-// var testes: any //qualquer tipo de variável
+//////////////////////////////
 
-// testes = 123
 
-// arrTestes = ["Guilherme, João"]
+var arrTestes: string[]
+var testes: any //qualquer tipo de variável
 
-// console.log(arrTestes[0]);
-// console.log(testes);
+testes = 123
 
-// /*-------------------------------*/
+arrTestes = ["Guilherme, João"]
 
-// function teste(valor:string){
-//     console.log(valor);
+console.log(arrTestes[0]);
+console.log(testes);
+
+
+/////////////////////////////
+
+
+function teste(valor:string){
+    console.log(valor);
     
-// }
+}
 
-// function somar(v1:number, v2:number){
-//     return v1 + v2
-// }
+function somar(v1:number, v2:number){
+    return v1 + v2
+}
 
-// teste(10)
-// console.log(somar(20,5));
+teste(10)// teste para erro no teminal
+console.log(somar(20,5));
+
+
 
 
 
