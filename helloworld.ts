8b955a9035db8c25f758e1f82d34cb8c640b4c39
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2022-07-21 21:29:27 -0300

    Update helloworld.ts

--- helloworld.ts
+++ helloworld.ts
@@ -1,40 +1,40 @@
-var nome:string = "Beto"
-var age:number
-var boolTest:boolean
+// var nome:string = "Beto"
+// var age:number
+// var boolTest:boolean
 
-boolTest = true
+// boolTest = true
 
-age = 23
+// age = 23
 
-console.log(nome);
-console.log(age);
-console.log(boolTest);
+// console.log(nome);
+// console.log(age);
+// console.log(boolTest);
 
-/*------------------------------*/
+// /*------------------------------*/
 
-var arrTestes: string[]
-var testes: any //qualquer tipo de variável
+// var arrTestes: string[]
+// var testes: any //qualquer tipo de variável
 
-testes = 123
+// testes = 123
 
-arrTestes = ["Guilherme, João"]
+// arrTestes = ["Guilherme, João"]
 
-console.log(arrTestes[0]);
-console.log(testes);
+// console.log(arrTestes[0]);
+// console.log(testes);
 
-/*-------------------------------*/
+// /*-------------------------------*/
 
-function teste(valor:string){
-    console.log(valor);
+// function teste(valor:string){
+//     console.log(valor);
     
-}
+// }
 
-function somar(v1:number, v2:number){
-    return v1 + v2
-}
+// function somar(v1:number, v2:number){
+//     return v1 + v2
+// }
 
-teste(10)
-console.log(somar(20,5));
+// teste(10)
+// console.log(somar(20,5));
 
 
 
